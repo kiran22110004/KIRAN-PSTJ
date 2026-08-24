2252c03f221f498b36733b38a632240ed8e3bbe2
kiran22110004 <vtu28629@veltech.edu.in>
2026-08-24 11:02:56 +0530

    Create Sorting comparator.java

---
+++ WEEK 1/SESSION 2/27-07-2026/WEEK-1/SESSION 3/27-07-2026/WEEK-1/Session-3/Sorting comparator.java
@@ -0,0 +1,61 @@
+// Program
+import java.io.*;
+import java.util.*;
+class Player {
+    String name;
+    int score;
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+class Checker implements Comparator<Player> {
+    public int compare(Player a, Player b) {
+        if (a.score != b.score) {
+            return b.score - a.score; // Higher score first
+        }
+        return a.name.compareTo(b.name); // Alphabetical order
+    }
+}
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        Player[] player = new Player[n];
+        for (int i = 0; i < n; i++) {
+            String name = scan.next();
+            int score = scan.nextInt();
+            player[i] = new Player(name, score);
+        }
+        Checker checker = new Checker();
+        Arrays.sort(player, checker);
+        for (Player p : player) {
+            System.out.println(p.name + " " + p.score);
+        }
+        scan.close();
+    }
+}
+
+
+Compiler Message: Success
+// Input (stdin):
+5
+amy 100
+david 100
+heraldo 50
+aakansha 75
+aleksa 150
+
+ Output:
+aleksa 150
+amy 100
+david 100
+aakansha 75
+heraldo 50
+  
+// Expected Output:
+aleksa 150
+amy 100
+david 100
+aakansha 75
+heraldo 50
